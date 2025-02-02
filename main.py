9d68b1a23269ee70717160cf58e5df572d848287
David Sullivan <dsulli8247@gmail.com>
2025-02-02 07:28:09 -0500

    Counts words of frankenstein.txt

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
-
+    
+    words = file_contents.split()
+    word_count = len(words)
+    print(word_count)
 
 
 main()
